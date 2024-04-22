3fc6d62a2e6601885465485ff65a87eae9d7c5f0
Manni Melgoza <emmanuelmelgoza1@gmail.com>
2024-04-21 23:02:12 -0600

    Update recursive-debug.js

--- problems/recursive-debug.js
+++ problems/recursive-debug.js
@@ -8,14 +8,27 @@ function.
 
 Examples:
 
-doForAll([], (x) => x * 2); // => []
-doForAll([1, 2, 3], (x) => x + 1); // => [2, 3, 4]
-doForAll(["a", "b", "c"], (x) => x.toUpperCase()); // => ["A", "B", "C"]
 ***********************************************************************/
 
+//Function is a function style that passes two parameters(array, function)
 function doForAll(arr, action) {
-  return [action(arr[0]), ...doForAll(arr.slice(1), action)];
-}
+  //BASE CASE:
+  //check if the length of the array is 0, when its true
+  if(arr.length === 0){
+    // the arr will be returned
+    return arr;
+  }
+  //Recursice case
 
+  // for every iteration that the if statement is not true
+  //The return of the recursion will take the first value in arr
+  //doForAll function will then slice the value and then call the action function
+  //By slicing it will make the arr being checked true. 
+  return [action(arr[0]), ...doForAll(arr.slice(1), action)];
+};
+
+console.log(doForAll([], (x) => x * 2)); // => []
+console.log(doForAll([1, 2, 3], (x) => x + 1)); // => [2, 3, 4]
+console.log(doForAll(["a", "b", "c"], (x) => x.toUpperCase())); // => ["A", "B", "C"]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = doForAll;
